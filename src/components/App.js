f5509353d0c4b0cad0a3f00d0931c3ab4437c743
hoil <qqwq9999@gmail.com>
2019-02-10 15:31:30 +0900

    react-router를 이용하여 간단한 어플리케이션 생성

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,21 @@
 import React from "react";
-import { hot } from 'react-hot-loader/root'
+import { hot } from "react-hot-loader/root";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-const App = () => <div>hoilzzzz</div>;
+import Home from "./Home";
+import AsyncPage from "./AsyncPage";
+import NoPage from "./NoPage";
+
+const App = () => (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/async" component={AsyncPage} />
+        <Route component={NoPage} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default hot(App);
