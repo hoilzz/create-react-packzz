9dcc8ec1046c425302b0049bc8d54db6a4ca0970
hoil <qqwq9999@gmail.com>
2019-02-18 02:39:38 +0900

    eslint-loaer 추가

--- boilerplate/webpack/webpack.dev.js
+++ boilerplate/webpack/webpack.dev.js
@@ -1,13 +1,18 @@
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   mode: 'development',
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
   devServer: {
-    contentBase: './dist',
+    contentBase: path.resolve(__dirname, '../dist'),
     hot: true,
     open: true,
     historyApiFallback: true,
+    publicPath: '/',
     overlay: true,
   },
   devtool: 'cheap-module-eval-source-map',
